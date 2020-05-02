776201e348af4e4fb7c32829c1001ff96d9b5343
Xiaoyu Yan <xy97@en-openmpi05.ece.cornell.edu>
2020-05-02 12:11:19 -0400

    added file structure

---
+++ src/gol_parallel.cu
@@ -0,0 +1,13 @@
+/**
+ * Xiaoyu Yan (xy97) 
+ * ECE 5720 Final Project Game of Life Parallel Version
+ * 
+ * To compile:
+ * 
+ * 
+ * */
+
+
+ int main(){
+   
+ }
---
+++ src/gol_seq.c
@@ -0,0 +1,12 @@
+/**
+ * Xiaoyu Yan (xy97) 
+ * ECE 5720 Final Project Game of Life Sequential Version
+ * 
+ * To compile:
+ * 
+ * 
+ * */
+
+int main(){
+  
+}
